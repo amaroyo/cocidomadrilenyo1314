9cd97d0f4fff707d958528c2f52ef5c336b019b2
eduarpla@gmail.com <eduarpla@gmail.com@250d828d-45c2-467d-f059-316c1f338299>
2014-01-08 19:06:21 +0000

    Algunos metodos de la clase User han sido desarrollados. Añadido campo de nombre en la clase Recipe.

--- trunk/project/LookCook/src/java/serverSide/Recipe.java
+++ trunk/project/LookCook/src/java/serverSide/Recipe.java
@@ -13,13 +13,19 @@ import java.util.ArrayList;
  * @author Asier
  */
 public class Recipe {
-    
+    private String recipeName;
     private ArrayList <String> ingredient;
     private String snippet;
     
     public Recipe () {
-        ingredient = new ArrayList ();
-        ingredient.clear ();
+        this.recipeName = "";
+        this.ingredient = new ArrayList ();
+        this.ingredient.clear ();
+    }
+    
+    public Recipe(String name, ArrayList <String> ingredient){
+        this.recipeName = name;
+        this.ingredient = ingredient;
     }
     
     public ArrayList getRecipeIngredients () {
@@ -36,6 +42,14 @@ public class Recipe {
     
     public void setRecipeSnippet (String snippet) {
         this.snippet = snippet;
+    }
+    
+    public String getRecipeName(){
+        return recipeName;
+    }
+    
+    public void setRecipeName(String name){
+        this.recipeName = name;
     }
     
     
