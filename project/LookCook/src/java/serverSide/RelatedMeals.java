bded4103083fb69b427b4cd1880c22ceef7b3136
eduarpla@gmail.com <eduarpla@gmail.com@250d828d-45c2-467d-f059-316c1f338299>
2014-01-10 16:11:11 +0000

    RelatedMeal completa con condiciones. Renombrada RelatedMeals.java. Sigue faltando el algoritmo para hacer combinaciones de n elementos en una coleccion de m elementos.

---
+++ project/LookCook/src/java/serverSide/RelatedMeals.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package serverSide;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Plasavall
+ */
+public class RelatedMeals {
+    
+    private ArrayList <Recipe> recipe;
+    private ArrayList <String> ingredient;
+    
+    public void updateRecipes(ArrayList <String> ingredients, int hits){
+        
+    }
+}
--- project/LookCook/src/java/serverSide/User.java
+++ project/LookCook/src/java/serverSide/User.java
@@ -26,7 +26,7 @@ public class User {
     private ArrayList <Ingredient> ingredient;
     private ArrayList <Recipe> recipe; 
     private Recommender recommender;
-    private RelatedMeal relatedMeal;
+    private RelatedMeals relatedMeal;
     
     public User (int id) {
         this.id = id;
@@ -38,7 +38,7 @@ public class User {
         recipe.clear ();
         
         recommender = new Recommender ();
-        relatedMeal = new RelatedMeal ();
+        relatedMeal = new RelatedMeals ();
         
     }
     
@@ -85,9 +85,42 @@ public class User {
         //Now we update the recipes section of the user using queryRecipes
         ArrayList <Recipe> recipesFound = new ArrayList<>();
         recipesFound.clear();
-        
+        //First we try the query with all the ingredients
         recipesFound = queryRecipes(clientIngredients);
+        if(recipesFound == null){
+            recipesFound = new ArrayList<>();
+            recipesFound.clear();
+        }
+        //If it doesn't retrieve anything, we try querying combinations of those
+        // until we get the recipes that contain the biggest quantity of the 
+        //requested ingredients possible.
+        int hits = clientIngredients.size()-1;
+        if(recipesFound.isEmpty()){
+            while(recipesFound.isEmpty()){
+                recipesFound = queryRecipes(clientIngredients, hits);
+                hits--;
+                if(recipesFound == null){
+                    recipesFound = new ArrayList<>();
+                    recipesFound.clear();
+                }
+            }
+        }
+        this.recipe = recipesFound;
+        //Now we send to the relatedmeals object the list of ingredients with
+        //the starting number of hits to calculate the even smaller combinations 
+        //of those ingredients and show them.
+        //this.relatedMeal.updateRecipes(clientIngredients, hits);
+        updateRelated(clientIngredients, hits);
+    }
+    
+    public void updateRelated(ArrayList <String> ingredients, int startHits){
+        ArrayList <Recipe> relatedRecipes = new ArrayList<>();
+        relatedRecipes.clear();
         
+        for(int hits = startHits; hits>0;hits--){
+            //This is supposed to concatenate both ArrayLists.
+            relatedRecipes.addAll(queryRecipes(ingredients,hits));
+        }
     }
     
     /**
