5ba94e05a6ba77926486bcef04138350b37a28d1
eduarpla@gmail.com <eduarpla@gmail.com@250d828d-45c2-467d-f059-316c1f338299>
2014-01-11 15:29:25 +0000

    Query class created to contain all the dbPedia query methods.

--- project/LookCook/src/java/serverSide/RelatedMeals.java
+++ project/LookCook/src/java/serverSide/RelatedMeals.java
@@ -14,10 +14,22 @@ import java.util.ArrayList;
  */
 public class RelatedMeals {
     
-    private ArrayList <Recipe> recipe;
-    private ArrayList <String> ingredient;
+    private ArrayList <Recipe> relatedRecipes;
     
-    public void updateRecipes(ArrayList <String> ingredients, int hits){
-        
+        /**
+         * Vamos a copiar y pegar las funciones de query a esta clase?
+         * 
+         * ¿Creamos una clase a parte para llamar a todos los query methods 
+         * estaticamente?
+         * 
+         * ¿Updateamos la lista de recetas en el user y la mandamos directamente
+         * aqui sin guardar copia en el user?
+         * 
+         * De momento voy a hacer esta ultima y ya veremos si la cambiamos o no
+         */
+    //public void updateRecipes(ArrayList <String> ingredients, int hits){ //Esta linea es paa la opcion 1 o 2
+    public void updateRecipes(ArrayList<Recipe> newRecipes){
+       this.relatedRecipes = newRecipes;
     }
+    
 }
