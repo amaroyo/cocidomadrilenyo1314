c150ba62a325f873d1d369b374e96bdf95f934ca
eduarpla@gmail.com <eduarpla@gmail.com@250d828d-45c2-467d-f059-316c1f338299>
2014-01-11 15:29:25 +0000

    Query class created to contain all the dbPedia query methods.

--- trunk/project/LookCook/src/java/serverSide/User.java
+++ trunk/project/LookCook/src/java/serverSide/User.java
@@ -16,10 +16,7 @@ import java.util.ArrayList;
  */
 public class User {
     
-    String SERVICE = "http://dbpedia.org/sparql";
-    String PREFIXES = "PREFIX dbo:<http://dbpedia.org/ontology/> "
-                      +"PREFIX dbres: <http://dbpedia.org/resource/> "
-                      +"PREFIX dbpprop: <http://dbpedia.org/property/> ";
+    
     
     
     private int id;
@@ -86,7 +83,7 @@ public class User {
         ArrayList <Recipe> recipesFound = new ArrayList<>();
         recipesFound.clear();
         //First we try the query with all the ingredients
-        recipesFound = queryRecipes(clientIngredients);
+        recipesFound = Query.recipes(clientIngredients);
         if(recipesFound == null){
             recipesFound = new ArrayList<>();
             recipesFound.clear();
@@ -97,7 +94,7 @@ public class User {
         int hits = clientIngredients.size()-1;
         if(recipesFound.isEmpty()){
             while(recipesFound.isEmpty()){
-                recipesFound = queryRecipes(clientIngredients, hits);
+                recipesFound = Query.recipes(clientIngredients, hits);
                 hits--;
                 if(recipesFound == null){
                     recipesFound = new ArrayList<>();
@@ -119,8 +116,9 @@ public class User {
         
         for(int hits = startHits; hits>0;hits--){
             //This is supposed to concatenate both ArrayLists.
-            relatedRecipes.addAll(queryRecipes(ingredients,hits));
+            relatedRecipes.addAll(Query.recipes(ingredients,hits));
         }
+        this.relatedMeal.updateRecipes(relatedRecipes);
     }
     
     /**
@@ -133,7 +131,7 @@ public class User {
         ArrayList<String> temp = new ArrayList<>();
         temp.clear();
         temp.add(ing);
-        ArrayList<Recipe> recipes = queryRecipes(temp);
+        ArrayList<Recipe> recipes = Query.recipes(temp);
         
         return !recipes.isEmpty();
     }
@@ -143,111 +141,5 @@ public class User {
     
     //Below this line there are the methods that make direct queries to dbPedia.
     
-    /**
-     * Queries a list of recipes given a list of ingredients.
-     * It's exclusive (Only the recipes containing at least that exact 
-     * ingredients are retrieved.
-     * @param ingredients
-     * @return a ResultSet of recipes containing the given list of ingredients
-     */
-    public ArrayList <Recipe> queryRecipes(ArrayList<String> ingredients){
-        ArrayList <Recipe> recipes = new ArrayList<>();
-        ArrayList <String> auxIngr = new ArrayList<>();
-        recipes.clear();
-        auxIngr.clear();
-        
-        String query=PREFIXES+"select ?recipe ?name_of_recipe where{";
-        for (String ing : ingredients) {
-            query += "?recipe dbo:ingredient dbres:" + ing + ".\n";
-        }
-        query+="?recipe dbpprop:name ?name_of_recipe.\n";
-        query+="}";
-        QueryExecution qe=QueryExecutionFactory.sparqlService(SERVICE, query);
-        ResultSet rs=qe.execSelect();
-        while(rs.hasNext()){
-            QuerySolution s=rs.nextSolution();
-            Resource recipeFound = s.getResource("?recipe");
-            Literal nameOfRecipe = s.getLiteral("?name_of_recipe");
-            auxIngr = queryIngredientsOfRecipe(recipeFound);
-            recipes.add(new Recipe(nameOfRecipe.getString(), auxIngr));
-        }
-        return recipes;
-    }
-    public ArrayList <Recipe> queryRecipes(ArrayList<String> ingredients, int hits){
-        
-        if(hits > ingredients.size()) return null;
-        if(hits == ingredients.size()) return queryRecipes(ingredients);
-        ArrayList <Recipe> recipesFound = new ArrayList<>();
-        ArrayList <String> auxIngr = new ArrayList<>();
-        if(hits == 1){
-            recipesFound.clear();
-            for(int i = 0; i < ingredients.size(); i++){
-                auxIngr.clear();
-                auxIngr.add(ingredients.get(i));
-                recipesFound.addAll(queryRecipes(auxIngr));
-            }
-            return recipesFound;
-        }
-        else{
-            ArrayList <Recipe> recipes = new ArrayList<>();
-            recipes.clear();
-            ArrayList <String> tmpIng = new ArrayList<>();
-            tmpIng.clear();
-            int i,j;
-            for (i = 0; i < ingredients.size() - hits + 1; i++){
-             /**
-             * Code goes here.
-             * We use queryRecipes(ArrayList<String> ingredients) with all the 
-             * possible combinations of as many ingredients as int hints 
-             * indicates.
-             */
-            }
-            
-            //Example of algorithm defined recursively in javascript:
-//var i, j, combs(recipes), head, tailcombs, set (ingredients), k (hits);
-//            if (k == 1) {
-//                    combs = [];
-//                    for (i = 0; i < set.length; i++) {
-//                            combs.push([set[i]]);
-//                    }
-//                    return combs;
-//            }
-//
-//            // Assert {1 < k < set.length}
-//
-//            combs = [];
-//            for (i = 0; i < set.length - k + 1; i++) {
-//                    head = set.slice(i, i+1);
-//                    tailcombs = k_combinations(set.slice(i + 1), k - 1);
-//                    for (j = 0; j < tailcombs.length; j++) {
-//                            combs.push(head.concat(tailcombs[j]));
-//                    }
-//            }
-//            return combs;
-            
-            return recipes;
-        }
-    }
-    /**
-     * 
-     * @param recipe is a dbresource
-     * @return an arrayList of Strings (ingredient names)
-     */
-    public ArrayList <String> queryIngredientsOfRecipe(Resource recipe){
-        ArrayList <String> ingredients = new ArrayList<>();
-        ingredients.clear();
-        String query=PREFIXES+"select ?ingredient_names where{";
-        query += recipe.toString()+" dbo:ingredient ?ingredients.\n"
-                    + "?ingredients dbpprop:name ?ingredient_names.\n";
-        query+="}";
-        QueryExecution qe=QueryExecutionFactory.sparqlService(SERVICE, query);
-        ResultSet rs=qe.execSelect();
-        while(rs.hasNext()){
-            QuerySolution s=rs.nextSolution();
-            Literal name=s.getLiteral("?ingredient_names");
-            ingredients.add(name.getString());            
-        }
-        return ingredients;
-            
-    }
+    
 }
