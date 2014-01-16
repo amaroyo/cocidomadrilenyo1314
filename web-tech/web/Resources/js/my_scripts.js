38024043ba686209bdfda8f94385c4c8df28f0fe
medastucorreo@gmail.com <medastucorreo@gmail.com@250d828d-45c2-467d-f059-316c1f338299>
2014-01-16 01:08:16 +0000

    Always invalid ingredient!!!

--- web-tech/web/Resources/js/my_scripts.js
+++ web-tech/web/Resources/js/my_scripts.js
@@ -30,13 +30,16 @@ function getUser(url) {
 function addIngredient() {
     var x,xx;
     if (xmlhttp.readyState===4) {
-        alert(xmlhttp.status);
-        if(xmlhttp.status===200) //PUT returning a response
-          {
+        if(xmlhttp.status===200) {//PUT returning a response
           x=xmlhttp.responseXML.getElementsByTagName("INGREDIENT")[0];
           xx=x.getElementsByTagName("PRODUCT")[0].firstChild.nodeValue;
-          document.getElementById("prueba").innerHTML=xx;
+          if(xx === "invalid") {
+              document.getElementById("InvalidIngredient").innerHTML = "Sorry that ingredient does not exist in DBPedia."
           }
+          else {
+            document.getElementById("InvalidIngredient").innerHTML=xx;
+          }
+        }
     }
 }
 
@@ -48,69 +51,6 @@ function putIngredient(url,ing) {
     xmlhttp.setRequestHeader("Content-type","application/xml");
     xmlhttp.send(xml);
 }
-
-
-/*function loadXMLDoc(url) {
-	var xmlhttp;
-	var txt,x,i;
-	if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-	  xmlhttp=new XMLHttpRequest();
-	  }
-	else
-	  {// code for IE6, IE5
-	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-	xmlhttp.onreadystatechange=function()
-	  {
-	  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-		{
-		
-		x=xmlhttp.responseXML.documentElement.getElementsByTagName("RECIPE");
-		txt='<ul class="media-list">';
-		for (i=0;i<x.length;i++)
-		  {
-			txt=txt + '<li class="media">';
-			txt=txt + '<a class="pull-left" href="#">' +
-					'<img class="media-object" src="Resources/icons/64x64/images.jpg">' + 
-					'</a><div class="media-body">' +
-					'<h4 class="media-heading">' +
-					x[i].getAttribute('NAME') + '</h4>' +
-					'<h5>'+ x[i].getElementsByTagName("INGREDIENT")[0].innerHTML +'</h5>'+		
-					'<p>' + x[i].getElementsByTagName("SNIPPET")[0].innerHTML + '</p>' +
-					'</div></li>';
-			
-		  }
-		txt=txt + '</ul>';
-		document.getElementById('txtRecetas').innerHTML=txt;
-		}
-	  };
-	xmlhttp.open("GET",url,true);
-	xmlhttp.send();
-}*/
-
-/*function newUser() {
-        
-        var xmlhttp;
-	if (window.XMLHttpRequest)
-	  {// code for IE7+, Firefox, Chrome, Opera, Safari
-	  xmlhttp=new XMLHttpRequest();
-	  }
-	else
-	  {// code for IE6, IE5
-	  xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
-	  }
-   
-	xmlhttp.onreadystatechange=function()
-	  {
-	  if (xmlhttp.readyState===4 && xmlhttp.status===200)
-		{
-		document.getElementById("IngredientButtonResult").innerHTML=xmlhttp.responseText;
-		}
-	  };
-        xmlhttp.open("GET","http://localhost:8080/web-tech/webresources/lookandcook/user",true);
-	xmlhttp.send();
-}*/
 
 
 var ingredients = ["almonds",
